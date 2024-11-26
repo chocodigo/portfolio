89daf683a6d01c4a7caed70dc95c8dfa0982ff69
chocodigo <chocodigo>
2024-11-26 19:45:46 +0900

    react hook form 도입중...

--- src/app/features/contact/components/ContactForm.tsx
+++ src/app/features/contact/components/ContactForm.tsx
@@ -1,37 +1,74 @@
 import MotionStack from "@/app/shared/components/MotionStack";
 import { Button, TextField } from "@mui/material";
 import SendIcon from "@mui/icons-material/Send";
+import { useForm, SubmitHandler } from "react-hook-form";
+
+type Inputs = {
+  name: string;
+  email: string;
+  contents: string;
+};
 
 export const ContactForm = () => {
+  const {
+    register,
+    handleSubmit,
+    watch,
+    formState: { errors },
+  } = useForm<Inputs>();
+  const onSubmit: SubmitHandler<Inputs> = (data) => console.log(data);
+
   return (
-    <MotionStack
-      sx={{
-        gap: "10px",
-        ".Mui-focused": {
-          color: "#CE9EAB",
-          "&::after": {
-            borderBottom: "2px solid #CE9EAB",
-          },
-        },
-      }}
-    >
-      <TextField required label="Your Name" variant="standard" />
-      <TextField required label="Your Email Address" variant="standard" />
-      <TextField
-        id="standard-multiline-static"
-        multiline
-        variant="filled"
-        rows={7}
-      />
-      <Button
-        startIcon={<SendIcon />}
-        variant={"contained"}
+    <form onSubmit={handleSubmit(onSubmit)}>
+      <MotionStack
         sx={{
-          background: "#CE9EAB",
+          gap: "10px",
+          ".Mui-focused": {
+            "&.MuiFormLabel-root": {
+              color: "#CE9EAB",
+            },
+            "&::after": {
+              borderBottom: "2px solid #CE9EAB",
+            },
+          },
         }}
       >
-        Send Message
-      </Button>
-    </MotionStack>
+        <TextField
+          required
+          label="Your Name"
+          variant="standard"
+          {...register("name")}
+        />
+        <TextField
+          required
+          label="Your Email Address"
+          variant="standard"
+          {...register("email", {
+            pattern: {
+              value: /^[^\s@]+@[^\s@]+\.[^\s@]+$/,
+              message: "Invalid email format",
+            },
+          })}
+        />
+        {errors.email && <p>{errors.email.message}</p>}
+        <TextField
+          id="standard-multiline-static"
+          multiline
+          variant="filled"
+          rows={7}
+          {...register("contents")}
+        />
+        <Button
+          type={"submit"}
+          startIcon={<SendIcon />}
+          variant={"contained"}
+          sx={{
+            background: "#CE9EAB",
+          }}
+        >
+          Send Message
+        </Button>
+      </MotionStack>
+    </form>
   );
 };
