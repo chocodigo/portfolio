30ee45c478ab723840417525a56cf6d25ff208a9
chocodigo <chocodigo>
2025-01-15 17:35:01 +0900

    feat: 이메일 성공 alert 추가

--- src/app/features/contact/api/useSendEmail.ts
+++ src/app/features/contact/api/useSendEmail.ts
@@ -1,6 +1,6 @@
 import { useMutation, UseMutationOptions } from "@tanstack/react-query";
 import { sendEmail } from "./sendEmail";
 
-export const useSendEmail = (onSuccess: () => void) => {
-  return useMutation({ mutationFn: sendEmail, onSuccess });
+export const useSendEmail = (onSuccess: () => void, onError: () => void) => {
+  return useMutation({ mutationFn: sendEmail, onSuccess, onError });
 };
--- src/app/features/contact/components/ContactForm.tsx
+++ src/app/features/contact/components/ContactForm.tsx
@@ -1,11 +1,13 @@
 "use client";
 
 import MotionStack from "@/app/shared/components/MotionStack";
-import { Button, TextField } from "@mui/material";
+import { Alert, TextField } from "@mui/material";
 import SendIcon from "@mui/icons-material/Send";
 import { useForm, SubmitHandler } from "react-hook-form";
 import { useSendEmail } from "../api/useSendEmail";
 import LoadingButton from "@mui/lab/LoadingButton";
+import Snackbar from "@mui/material/Snackbar";
+import { useState } from "react";
 
 type Inputs = {
   name: string;
@@ -20,71 +22,112 @@ export const ContactForm = () => {
     reset,
     formState: { errors },
   } = useForm<Inputs>();
-
-  const { mutate, isPending } = useSendEmail(() => {
-    console.log("success");
-    reset();
+  const [openAlert, setOpenAlert] = useState({
+    open: false,
+    isSuccess: false,
+    message: "",
   });
+
+  const onSuccess = () => {
+    setOpenAlert({
+      open: true,
+      isSuccess: true,
+      message: "메일을 보냈습니다.",
+    });
+    reset();
+  };
+
+  const onError = () => {
+    setOpenAlert({
+      open: true,
+      isSuccess: false,
+      message: "메일 보내기에 실패했습니다.",
+    });
+  };
+
+  const handleClose = () => {
+    setOpenAlert({
+      ...openAlert,
+      open: false,
+    });
+  };
+
+  const { mutate, isPending } = useSendEmail(onSuccess, onError);
 
   const onSubmit: SubmitHandler<Inputs> = (data) => {
     mutate(data);
   };
 
   return (
-    <form onSubmit={handleSubmit(onSubmit)}>
-      <MotionStack
-        sx={{
-          gap: "10px",
-          ".Mui-focused": {
-            "&.MuiFormLabel-root": {
-              color: "#CE9EAB",
-            },
-            "&::after": {
-              borderBottom: "2px solid #CE9EAB",
-            },
-          },
-        }}
-      >
-        <TextField
-          required
-          label="Your Name"
-          variant="standard"
-          {...register("name")}
-        />
-        <TextField
-          required
-          label="Your Email Address"
-          variant="standard"
-          {...register("email", {
-            pattern: {
-              value: /^[^\s@]+@[^\s@]+\.[^\s@]+$/,
-              message: "Invalid email format",
-            },
-          })}
-          error={!!errors.email}
-          helperText={errors.email?.message || ""}
-        />
-        {errors.email && <p>{errors.email.message}</p>}
-        <TextField
-          id="standard-multiline-static"
-          multiline
-          variant="filled"
-          rows={7}
-          {...register("message")}
-        />
-        <LoadingButton
-          type={"submit"}
-          startIcon={<SendIcon />}
-          varia-nt={"contained"}
+    <>
+      <form onSubmit={handleSubmit(onSubmit)}>
+        <MotionStack
           sx={{
-            background: "#CE9EAB",
-            color: "white",
+            gap: "10px",
+            ".Mui-focused": {
+              "&.MuiFormLabel-root": {
+                color: "#CE9EAB",
+              },
+              "&::after": {
+                borderBottom: "2px solid #CE9EAB",
+              },
+            },
           }}
-          loading={isPending}
         >
-          Send Message
-        </LoadingButton>
-      </MotionStack>
-    </form>
+          <TextField
+            required
+            label="이름"
+            variant="standard"
+            {...register("name")}
+          />
+          <TextField
+            required
+            label="이메일 주소"
+            variant="standard"
+            {...register("email", {
+              pattern: {
+                value: /^[^\s@]+@[^\s@]+\.[^\s@]+$/,
+                message: "Invalid email format",
+              },
+            })}
+            error={!!errors.email}
+            helperText={errors.email?.message || ""}
+          />
+          {errors.email && <p>{errors.email.message}</p>}
+          <TextField
+            id="standard-multiline-static"
+            multiline
+            variant="filled"
+            rows={7}
+            {...register("message")}
+          />
+          <LoadingButton
+            type={"submit"}
+            startIcon={<SendIcon />}
+            varia-nt={"contained"}
+            sx={{
+              background: "#CE9EAB",
+              color: "white",
+            }}
+            loading={isPending}
+          >
+            메일 보내기
+          </LoadingButton>
+        </MotionStack>
+      </form>
+      <Snackbar
+        open={openAlert.open}
+        autoHideDuration={6000}
+        onClose={handleClose}
+      >
+        <Alert
+          severity={openAlert.isSuccess ? "success" : "error"}
+          variant="filled"
+          sx={{ width: "100%" }}
+        >
+          {openAlert.message}
+        </Alert>
+      </Snackbar>
+    </>
   );
 };
